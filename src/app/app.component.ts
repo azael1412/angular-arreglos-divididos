b9f4ee461b2544e4207ad3cd3bc4a5b197dbde92
azael1412 <azael_1412@hotmail.com>
2019-12-09 16:06:13 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
-  @ViewChildren('table') tables:QueryList<ElementRef>;
+  @ViewChildren('table') tables :QueryList<ElementRef>;
   events = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
   total : number = 0;
   data : any = [];
   totalData : number = 0;
    constructor(){
     this.events = this.chunks(this.events,8);
+    console.log("datos: ",this.events)
   }
   ngAfterViewInit(){
     // print array of CustomComponent objects
@@ -31,9 +32,9 @@ export class AppComponent  {
         results.push(array.splice(0, size));
         }
         this.data = results;
-        this.totalData = this.data.length;
-        console.log("total de datos del array: "+this.totalData)
-        console.log(this.data)
+        // this.totalData = this.data.length;
+        // console.log("total de datos del array: "+this.totalData)
+        // console.log(this.data)
         return results;
   }
 }
